f5fc23dbbe827247d0c25406c3d348e98d76032e
Areyes <areyez92@gmail.com>
2023-10-21 14:56:49 -0400

    S

--- lab2/part12.c
+++ lab2/part12.c
@@ -1,112 +1,193 @@
-#include <stdio.h>
-#include <time.h>
-#include <semaphore.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 
-#define buffersize 10
-#define NUM_PROVIDER 2
-#define NUM_BUYER 260
+// defining QUEUE size
+#define size 10
+int t7;
+int r = 0;
+int count;
+// Thread callback function
+void *producer(void *args);
+void *consumer(void *args);
 
-pthread_mutex_t mutex;
-pthread_t tidP[NUM_PROVIDER], tidB[NUM_BUYER];
-sem_t full, empty;
-
-int counter;
-int buffer[buffersize];
-
-void initialize(){
-    pthread_mutex_init(&mutex, NULL);
-
-    sem_init(&full, 1, 0);
-
-    sem_init(&empty, 1, buffersize);
-
-    counter = 0;
-}
-
-void write(int item)
-
-{
-    buffer[counter++] = item;
-}
-
-int read()
-
+typedef struct
 {
 
-    return (buffer[--counter]);
-}
+    int buffer[size];        // buffer size
+    long h1, t1;             // head,tail
+    int full, empty;         // full empty flags
+    pthread_mutex_t *mutex;  // mutex locks
+    pthread_cond_t *nF, *nE; // pointer for not full,not empty
+} queue;
 
-void *provider(void *param)
+queue *queueInit(void);             // intialised queue
+void qdelete(queue *q);             // delete item from queue
+void qadd(queue *q, int in);        // add item to queue
+void qdeletet1(queue *q, int *out); // delete from queue
 
+int main(int argc, char **argv) // Main program
 {
-
-    int waittime, item, i;
-
-    srand(time(NULL));
-
-    item = rand() % 100;
-
-    waittime = rand() % 5;
-
-    sem_wait(&empty);
-
-    pthread_mutex_lock(&mutex);
-
-    printf("\nProvider has added item: %d\n", item);
-
-    write(item);
-
-    pthread_mutex_unlock(&mutex);
-
-    sem_post(&full);
-}
-
-void *buyer(void *param)
-
-{
-
-    int waittime, item;
-
-    waittime = rand() % 5;
-
-    sem_wait(&full);
-
-    pthread_mutex_lock(&mutex);
-
-    item = read();
-
-    printf("\nBuyer has bought item: %d\n", item);
-
-    pthread_mutex_unlock(&mutex);
-
-    sem_post(&empty);
-}
-
-int main()
-
-{
-
-    int i;
-
-    initialize();
-
-    for (i = 0; i < NUM_PROVIDER; i++)
-
-        pthread_create(&tidP[i], NULL, provider, NULL);
-
-    for (i = 0; i < NUM_BUYER; i++)
-
-        pthread_create(&tidB[i], NULL, buyer, NULL);
-
-    for (i = 0; i < NUM_PROVIDER; i++)
-
-        pthread_join(tidP[i], NULL);
-
-    for (i = 0; i < NUM_BUYER; i++)
-
-        pthread_join(tidB[i], NULL);
-
+    // t7=argv[1]+1; //uncomment to use it with command line input
+    printf("Enter the queue size: "); // user input
+    scanf("%d", &t7);                 ////user input
+    queue *f;                         // queue variable
+    pthread_t produce, consume;
+    f = queueInit(); // QUEUE initialise
+    if (f == NULL)   // if thread is not initialised
+    {
+        fprintf(stderr, "main: Queue Initialisation failed.n");
+        exit(1);
+    }
+    count = pthread_create(&produce, NULL, producer, f); // producer thread created
+    if (count)                                           // if producer thread is not created
+    {
+        printf("n ERROR : pthread_create %d ", count);
+        exit(-1);
+    }
+    count = pthread_create(&consume, NULL, consumer, f); // consumer thread created
+    if (count)                                           // if consumer thread is not created
+    {
+        printf("n ERROR : pthread_create %d ", count);
+        exit(-1);
+    }
+    count = pthread_join(produce, NULL); // join producer thread to main thread
+    if (count)                           // if joins fail
+    {
+        printf("n ERROR : pthread_create %d ", count);
+        exit(-1);
+    }
+    count = pthread_join(consume, NULL); // join consumer thread to main thread
+    if (count)                           // if joins fail
+    {
+        printf("n ERROR : pthread_create %d ", count);
+        exit(-1);
+    }
+    qdelete(f);
     return 0;
+}
+void *producer(void *q) // producer thread
+{
+    queue *f; // queue creation
+    int i;
+    f = (queue *)q; // queue variable
+    for (i = 0; i < t7; i++)
+    {
+        r++;
+        pthread_mutex_lock(f->mutex); // taking lock on mutex
+        while (f->full)               // wait untill the space is not created
+        {
+            printf("producer: queue is full.n");
+            pthread_cond_wait(f->nF, f->mutex);
+        }
+        qadd(f, i);                     // add element to queue
+        pthread_mutex_unlock(f->mutex); // accuire the lock
+        pthread_cond_signal(f->nE);     // signal the block consumer
+        printf("(producer) Putting %d : %d items in the queue.n", i, r);
+        usleep(100000);
+    }
+
+    return (NULL);
+}
+void *consumer(void *q) // consumer thread
+{
+    queue *f; // queue creation
+    int i, d;
+
+    f = (queue *)q; // queue variable
+    for (i = 0; i < t7; i++)
+    {
+        r--;
+        pthread_mutex_lock(f->mutex); // taking lock on mutex of buffer
+        while (f->empty)              // wait until one item added to buffer
+        {
+            printf("consumer: queue is empty.n");
+            pthread_cond_wait(f->nE, f->mutex); // signal the block producer to add item
+        }
+        qdeletet1(f, &d);               // remove item from queue
+        pthread_mutex_unlock(f->mutex); // acquire lock
+        pthread_cond_signal(f->nF);
+        printf("(consumer) Getting %d : %d items in the queue.n", d, r);
+        usleep(500000);
+    }
+
+    return (NULL);
+}
+
+#ifdef o
+typedef struct
+{
+    int buffer[size];
+    long h1, t1;
+    int full, empty;
+    pthread_mutex_t *mutex;
+    pthread_cond_t *nF, *nE;
+} queue;
+#endif
+
+queue *queueInit(void) // intialise the queue
+{
+    queue *q;                           // queue variable
+    q = (queue *)malloc(sizeof(queue)); // creating a queue
+    if (q == NULL)
+        return (NULL);                                             // empty queue
+    q->empty = 1;                                                  // flags
+    q->full = 0;                                                   // flags
+    q->h1 = 0;                                                     // flags
+    q->t1 = 0;                                                     // flags
+    q->mutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t)); // mutex initialised
+    count = pthread_mutex_init(q->mutex, NULL);                    // if mutex is not initialised
+    if (count)
+    {
+        printf("n ERROR : pthread_mutex_init %d ", count);
+        exit(-1);
+    }
+
+    q->nF = (pthread_cond_t *)malloc(sizeof(pthread_cond_t)); // update non Full pointer
+    pthread_cond_init(q->nF, NULL);
+    q->nE = (pthread_cond_t *)malloc(sizeof(pthread_cond_t)); // update non empty pointer
+    pthread_cond_init(q->nE, NULL);
+    return (q);
+}
+
+void qdelete(queue *q)
+{
+    count = pthread_mutex_destroy(q->mutex); // mutex destroy
+    if (count)                               // if mutex is not destroy
+    {
+        printf("n ERROR : pthread_mutex_destroy %d ", count);
+        exit(-1);
+    }
+    free(q->mutex); // delete mutex
+    pthread_cond_destroy(q->nF);
+    free(q->nF);
+    pthread_cond_destroy(q->nE);
+    free(q->nE); // delete non empty pointer
+    free(q);     // delete queue
+}
+
+void qadd(queue *q, int in)
+{
+    q->buffer[q->t1] = in; // buffer adding of item
+    q->t1++;               // update read
+    if (q->t1 == size)     // check front reached size of buffer read
+        q->t1 = 0;         // empty
+    if (q->t1 == q->h1)    // check rear reached size of buffer front
+        q->full = 1;       // full
+    q->empty = 0;
+    return;
+}
+
+void qdeletet1(queue *q, int *out)
+{
+
+    *out = q->buffer[q->h1]; // buffer delete
+    q->h1++;                 // update front
+    if (q->h1 == size)       // front reach to rear
+        q->h1 = 0;           // empty
+    if (q->h1 == q->t1)      // read reach to front
+        q->empty = 1;        // full
+    q->full = 0;
+    return;
 }
