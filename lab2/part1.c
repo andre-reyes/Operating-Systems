15900101cc3228c0b447131bf58d865f5390f6ce
Areyes <areyez92@gmail.com>
2023-10-21 15:15:58 -0400

    changed it up

--- lab2/part1.c
+++ lab2/part1.c
@@ -4,46 +4,47 @@
 #include <unistd.h>
 
 #define PROVIDER_THREAD_COUNT 2    // provider thread count
-#define BUYER_THREAD_COUNT atoi(argv[1]) // buyer thread count command-line argument
+#define BUYER_THREAD_COUNT  atoi(argv[1])// buyer thread count command-line argument
 #define BUFFER 20      // maximum size of the queue
 
 int queue[BUFFER];
 int buffer = 0;
-pthread_cond_t queue_empty = pthread_cond_init;
+pthread_cond_t queue_available = pthread_cond_init;
 pthread_cond_t queue_full = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t queue_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *provider_insert(void *arg);
 void *buyer_remove(void *arg); 
 
-int main(int argc, char **argv)
+int main(int argc, char *argv)
 {
     int i;
 
-    int provider_ids[PROVIDER_THREAD_COUNT];
+    int provider_ids[2];
     int buyer_ids[BUYER_THREAD_COUNT];
     pthread_t providers[PROVIDER_THREAD_COUNT];
     pthread_t buyers[BUYER_THREAD_COUNT];
 
     if (argc != 2)
     {
-        fprintf(stderr, "Usage: %s <number of buyers>\n", argv[0]);
+        fprintf("Format must be:  [file location] <int>\n", argv[0]);
         exit(0);
     }
 
     // create provider threads
-    for (i = 0; i < PROVIDER_THREAD_COUNT; i++)
-    {
-        provider_ids[i] = i;
-        pthread_create(&providers[i], NULL, provider_insert, (void *)&provider_ids[i]);
-    }
+    
+    provider_id[0] = "Provider A";
+    provider_id[1] = "Provider B";
+    pthread_create(&providers[0], NULL, provider_insert, &provider_ids[0]);
+    pthread_create(&providers[1], NULL, provider_insert, &provider_ids[1]);
+    
 
     // create buyer threads
     for (i = 0; i < BUYER_THREAD_COUNT; i++)
     {
         buyer_ids[i] = i;
         pthread_create(&buyers[i], NULL,
-                       buyer_remove, (void *)&buyer_ids[i]);
+                       buyer_remove, &buyer_ids[i]);
     }
 
     // wait for the provider and buyer threads to finish
@@ -65,18 +66,18 @@ void *provider_insert(void *arg)
     while (1)
     {
         // produce an item
-        item = rand() % atoi(argv[1]);
+        item = rand() % 100 + 1; // produce item 1-100
         pthread_mutex_lock(&queue_mutex);
 
         // wait until the queue is not full
         while (buffer == BUFFER)
         {
-            pthread_cond_wait(&queue_empty, &queue_mutex);
+            pthread_cond_wait(&queue_available, &queue_mutex);
         }
 
         // insert the item into the queue
         queue[buffer++] = item;
-        printf("Provider %d produced item %d\n", *((int *)arg), item);
+        printf("Provider %s produced item %d\n", *((int *)arg), item);
 
         pthread_cond_signal(&queue_full);
         pthread_mutex_unlock(&queue_mutex);
@@ -84,8 +85,6 @@ void *provider_insert(void *arg)
         // sleep
         sleep(1);
     }
-
-    return NULL;
 }
 
 void *buyer_remove(void *arg)
@@ -105,12 +104,10 @@ void *buyer_remove(void *arg)
         item = queue[--buffer];
         printf("Buyer %d bought item %d\n", *((int *)arg), item);
 
-        pthread_cond_signal(&queue_empty);
+        pthread_cond_signal(&queue_available);
         pthread_mutex_unlock(&queue_mutex);
 
         // sleep
         sleep(1);
     }
-
-    return NULL;
 }
