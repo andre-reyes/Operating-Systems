52d6eeba212c6b7d58317dfa9170b39fa3f2a149
Andre <areyez92@gmail.com>
2023-10-21 16:37:59 -0400

    syntax

--- lab2/part1.c
+++ lab2/part1.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
+#include <signal.h>
 #include <unistd.h>
 
 #define BUYER_THREAD_COUNT  atoi(argv[1])// buyer thread count command-line argument
-#define BUFFER_SIZE 20      // maximum size of the queue
+#define BUFFER_SIZE 10      // maximum size of the queue
 
 int queue[BUFFER_SIZE];
-int buffer = 0;
+int buffer_counter = 0;
 pthread_cond_t queue_available = PTHREAD_COND_INITIALIZER;
 pthread_cond_t queue_full = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t queue_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t()
 
 void *provider_insert(void *arg);
 void *buyer_remove(void *arg); 
 
 char provider1_id[] = "Provider A";
-char provider2_id[] = "Provider B";
+char provider2_id[] = " Provider B";
 
 sem_t bin_sem;	// semaphore
 
@@ -38,7 +39,7 @@ int main(int argc, char **argv)
     }
 
     //semaphore initialization, first value = 0
-    state = sem_init(&bin_sem, 0 ,0);
+    int state = sem_init(&bin_sem, 0 ,0);
     if(state!=0)
     puts("Error semaphore initialization!!!");
 
@@ -61,7 +62,7 @@ int main(int argc, char **argv)
     }
     
     sem_destroy(&bin_sem);	// destroy semaphore
-    pthread_mutex_destroy(&mutx);	// destroy mutex
+    pthread_mutex_destroy(&queue_mutex);	// destroy mutex
 
     return 0;
 }
@@ -69,20 +70,20 @@ int main(int argc, char **argv)
 void *provider_insert(void *arg)
 {
     int item;
-    while (1)
+    for(int i=0;i<BUFFER_SIZE;i++)
     {
         // produce an item
-        item = rand() % 100 + 1; // produce item 1-100
+        item = rand() % 10 + 1; // produce item 1-100
         pthread_mutex_lock(&queue_mutex);
 
         // wait until the queue is not full
-        while (buffer == BUFFER_SIZE)
+        while (buffer_counter  == BUFFER_SIZE)
         {
             pthread_cond_wait(&queue_available, &queue_mutex);
         }
 
         // insert the item into the queue
-        queue[buffer++] = item;
+        queue[buffer_counter ++] = item;
         printf("%s produced item %d\n", (char*)arg, item);
         sem_post(&bin_sem);	// semaphore to increase
 
@@ -92,23 +93,24 @@ void *provider_insert(void *arg)
         // sleep
         sleep(1);
     }
+    return NULL;
 }
 
 void *buyer_remove(void *arg)
 {
     int item;
-    while (1)
+    for(int i=0;i<BUFFER_SIZE;i++)
     {
         sem_wait(&bin_sem);	//decrease index_counter
         pthread_mutex_lock(&queue_mutex);
         // wait until the queue is not empty
-        while (buffer == 0)
+        while (buffer_counter  == 0)
         {
             pthread_cond_wait(&queue_full, &queue_mutex);
         }
 
         // buy an item from the queue
-        item = queue[--buffer];
+        item = queue[--buffer_counter ];
         printf("Buyer %d bought item %d\n", *((int *)arg), item);
 
         pthread_cond_signal(&queue_available);
@@ -117,4 +119,5 @@ void *buyer_remove(void *arg)
         // sleep
         sleep(1);
     }
+    return NULL;
 }
